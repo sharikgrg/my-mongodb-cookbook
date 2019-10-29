1d0af7a56e93ed8a99610656f004201a705393d5
Sharik Gurung <SGurung@spartaglobal.com>
2019-10-29 15:51:00 +0000

    mongodb cookbook with all the datas

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -10,20 +10,39 @@ describe 'mongo::default' do
   context 'When all attributes are default, on Ubuntu 18.04' do
     # for a complete list of available platforms and versions see:
     # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'ubuntu', '18.04'
+    platform 'ubuntu', '16.04'
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'should update source list' do
+      expect(chef_run).to update_apt_update 'update_sources'
     end
+
+    it 'should add mongod to source list' do
+      expect(chef_run).to add_apt_repository('mongodb-org')
+    end
+
+    it 'should install mongodb' do
+      expect(chef_run).to install_package 'mongodb-org'
+    end
+
+    it 'should conf file in /etc/mongod.conf' do
+      expect(chef_run).to create_template "/etc/mongod.conf"
+    end
+
+    it 'should create mongod.service template in /etc/systemd/system/mongod.service' do
+      expect(chef_run).to create_template '/etc/systemd/system/mongod.service'
+    end
+
+    it 'enables mongod service' do
+      expect(chef_run).to enable_service 'mongod'
+    end
+
+    it 'starts mongod service' do
+      expect(chef_run).to start_service 'mongod'
+    end
+
   end
 end
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -11,6 +11,17 @@ unless os.windows?
 end
 
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe port('0.0.0.0', 27017) do
+  it { should be_listening }
+end
+
+describe package "mongodb-org" do
+  it { should be_installed }
+  its('version') { should match /3\./}
+end
+
+describe service "mongod" do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running}
 end
